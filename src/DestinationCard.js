6f79bc86d1281a253cbdff1c9f972596d24c3f7a
Malavika S <malavikas343@gmail.com>
2025-01-25 19:14:36 +0530

    Create DestinationCard.js

---
+++ src/DestinationCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function DestinationCard({ destination, index, removeDestination }) {
+  return (
+    <div className="destination-card">
+      <h3>{destination.name}</h3>
+      <p>{destination.notes}</p>
+      <p className={`priority-${destination.priority.toLowerCase()}`}>
+        Priority: {destination.priority}
+      </p>
+      {destination.image && <img src={destination.image} alt={destination.name} />}
+      <button onClick={() => removeDestination(index)}>Remove</button>
+    </div>
+  );
+}
+
+export default DestinationCard;
