6f187c1a68077d118af2500bfe050c1c88190679
Malavika S <malavikas343@gmail.com>
2025-01-26 00:13:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import DestinationForm from "./DestinationForm";
 import DestinationList from "./DestinationList";
+import Slideshow from './Slideshow';  
+import './styles.css';
+import './Slideshow.css';
 
 function App() {
   const [destinations, setDestinations] = useState([]);
 
-  // Load destinations from localStorage when the component mounts
+   
   useEffect(() => {
     const storedDestinations = JSON.parse(localStorage.getItem("destinations"));
     if (storedDestinations) {
@@ -13,17 +16,17 @@ function App() {
     }
   }, []);
 
-  // Save destinations to localStorage when the list changes
+   
   useEffect(() => {
     localStorage.setItem("destinations", JSON.stringify(destinations));
   }, [destinations]);
 
-  // Function to add a new destination
+   
   const addDestination = (destination) => {
     setDestinations((prevDestinations) => [...prevDestinations, destination]);
   };
 
-  // Function to remove a destination
+   n
   const removeDestination = (index) => {
     const newDestinations = destinations.filter((_, i) => i !== index);
     setDestinations(newDestinations);
@@ -32,6 +35,10 @@ function App() {
   return (
     <div className="container">
       <h1>Travel Bucket List Planner</h1>
+      
+      {/* Add the Slideshow component */}
+      <Slideshow /> {/* Place it where you'd like in your JSX */}
+      
       <DestinationForm addDestination={addDestination} />
       <DestinationList destinations={destinations} removeDestination={removeDestination} />
     </div>
