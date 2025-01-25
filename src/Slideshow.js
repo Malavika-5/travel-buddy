50a522f3af7609f7984e377831e4627af672dacf
Malavika S <malavikas343@gmail.com>
2025-01-26 00:48:04 +0530

    Update Slideshow.js

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,31 +1,31 @@
- 
-import React, { useState } from "react";
-import './Slideshow.css';  
+import React, { useState } from 'react';
+//import './Slideshow.css'; // Ensure you import the CSS file
 
 const Slideshow = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
   const panels = [
     {
-      image: "https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      title: "Explore The World"
+      title: 'Explore The World',
+      imageUrl: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     },
     {
-      image: "https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      title: "Wild Forest"
+      title: 'Wild Forest',
+      imageUrl: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     },
     {
-      image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80",
-      title: "Sunny Beach"
+      title: 'Sunny Beach',
+      imageUrl: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
     },
     {
-      image: "https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
-      title: "City on Winter"
+      title: 'City on Winter',
+      imageUrl: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
     },
     {
-      image: "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      title: "Mountains - Clouds"
-    }
+      title: 'Mountains - Clouds',
+      imageUrl: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    },
   ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleClick = (index) => {
     setActiveIndex(index);
@@ -37,7 +37,7 @@ const Slideshow = () => {
         <div
           key={index}
           className={`panel ${activeIndex === index ? 'active' : ''}`}
-          style={{ backgroundImage: `url(${panel.image})` }}
+          style={{ backgroundImage: `url(${panel.imageUrl})` }}
           onClick={() => handleClick(index)}
         >
           <h3>{panel.title}</h3>
